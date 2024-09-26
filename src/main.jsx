2ed4afc9baa209ec5aaf8b6f43e56be6bd0e2c18
Aniket <sinhaaniket192@gmail.com>
2024-09-26 12:40:37 +0530

    next

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import Result from './Result.jsx'
+import './index.css'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
